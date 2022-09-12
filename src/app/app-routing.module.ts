bf0b32a32e27b7915f7f7e4f91b559c9aa736214
Naman Jain <44242805+namanjainyr@users.noreply.github.com>
2022-09-13 04:46:17 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,19 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './Pages/main/main.component';
 import { PayTransactionComponent } from './Pages/transaction/pay-transaction/pay-transaction.component';
 import { ReceiveTransactionComponent } from './Pages/transaction/receive-transaction/receive-transaction.component';
-import { SignupComponent } from './Pages/user-account/signup/signup.component';
 import { HelpComponent } from './Pages/help/help.component';
 import { CareersComponent } from './Pages/careers/careers.component';
 import { PageNotFoundComponent } from './Pages/error/page-not-found/page-not-found.component';
-import { UserAccountComponent } from './pages/user-account/user-account.component';
 import { TransactionComponent } from './Pages/transaction/transaction.component';
 
 const appRoutes: Routes = [
     { path: '', component: MainComponent},
-    { path: 'login', component: UserAccountComponent},
     { path: 'pay', component: PayTransactionComponent},
     { path: 'receive', component: ReceiveTransactionComponent},
-    { path: 'signup', component: SignupComponent},
     { path: 'help', component: HelpComponent},
     { path: 'careers', component: CareersComponent},
     { path: 'transaction', component: TransactionComponent},
